4e341408101767529c9e7f6705d8d5ee3883f537
Yunas Magsi <yunasmagsi@cmail.carleton.ca>
2023-06-13 17:06:25 -0400

    small updates

--- script/script.js
+++ script/script.js
@@ -17,3 +17,17 @@ buttons.forEach(button => {
   })
 })
 
+let tabs = document.querySelectorAll(".tabs h3");
+let tabContents = document.querySelectorAll(".tab-content div");
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabContents.forEach((content) => {
+      content.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tabContents[index].classList.add("active");
+    tabs[index].classList.add("active");
+  });
+});
